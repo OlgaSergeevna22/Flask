725348f31728a94fd21e8c5b660403074f74237b
Ольга <devyatovka.0993@mail.ru>
2022-07-12 23:47:05 +0300

    create_model_Article

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
 db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullabel=False)
+    intro = db.Column(db.String(300), nullabel=False)
+    text = db.Column(db.Text, nullabel=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r' %self.id
 
 @app.route('/')
 def index():
