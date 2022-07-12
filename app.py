cba5571c913dd9b8ad5d2ac93560d2f1473dc2a9
Ольга <devyatovka.0993@mail.ru>
2022-07-12 18:05:58 +0300

    Flask_start

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/about')
+def about():
+    return 'About me'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page' + name + '-' + id
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
