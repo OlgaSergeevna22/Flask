11095e8400dba83dc69f7313261c1e94c8ebe132
Ольга <devyatovka.0993@mail.ru>
2022-07-13 12:59:04 +0300

    data_method_POST

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,7 +17,8 @@ class Article(db.Model):
     updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
-        return '<Article %r' %self.id
+        return '<Article %r' % self.id
+
 
 @app.route('/')
 def index():
@@ -26,9 +29,25 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/create-article')
+
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
-    return render_template('create-article.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
+    else:
+        return render_template('create-article.html')
+
 
 @app.route('/homework')
 def homework():
